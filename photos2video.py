dc82239da7f6b33a1e4eba4374c00104aed51134
rguidice <rjgnospam@gmail.com>
2021-01-05 17:31:41 -0700

    Added option for variable image sizes.

--- photos2video.py
+++ photos2video.py
@@ -4,12 +4,6 @@ import os
 import argparse
 import cv2
 
-### TODO: ###
-#research ways to deal with different sized images
-    #make video size the size of largest image in array
-        #only do this if argument is provided for better performance
-#add in file extension to output argument if user doesn't
-
 # Error checking for codec argument parsing
 def codec_checker(arg):
     if str(arg) not in ['DIVX', 'mp4v']:
@@ -25,20 +19,24 @@ parser.add_argument('-dir', '--directory', dest = 'dir', type = str,
 parser.add_argument('-ext', '--extension', dest = 'ext', type = str,
     default = 'any', help = ('File extension of image files. Supported '
     'extensions can be found in the OpenCV docs. Default = any.'))
+parser.add_argument('-var', '--var_image_size', dest = 'var',
+    action = 'store_true', help = ('Variable image size. Check README for more '
+    'info.'))
 parser.add_argument('-fr', '--framerate', dest = 'fr', type = int,
     default = 30, help = 'Frame rate of output video as int. Default = 30.')
 parser.add_argument('-c', '--codec', dest = 'codec', type = codec_checker,
     default = 'DIVX', help = ('Video codec of output file. Options are "DIVX" '
     '(output extension of .avi) or "mp4v" (output extension of .mp4) Default '
     '= DIVX.'))
-parser.add_argument('-out', '--output', dest = 'out', type = str,
+parser.add_argument('-out', '--output', dest = 'out', type = str, default = 'output',
     help = ('Output video file name. Can take both relative and absolute file '
-    'paths.'))
+    'paths. Default = output.'))
 args = parser.parse_args()
 
 # Gather inputted arguments from argument parser
 dir = args.dir
 ext = args.ext
+var = args.var
 fr = args.fr
 codec = args.codec
 output = args.out
@@ -68,18 +66,38 @@ for file in os.listdir(dir):
 if len(images) == 0:
     raise RuntimeError("No images found in directory provided")
 
-# Get video size data from first image in array
-first_image = cv2.imread(os.path.join(dir, images[0]))
-height, width, layers = first_image.shape
-video_size = (width, height)
+# Get video size data from largest image in array if var
+# Get video size data from first image in array if !var
+max_width = 0
+max_height = 0
+
+if var:
+    for img in images:
+        img_cv = cv2.imread(os.path.join(dir, img))
+        height, width, layers = img_cv.shape
+        if width > max_width:
+            max_width = width
+        if height > max_height:
+            max_height = height
+else:
+    first_image = cv2.imread(os.path.join(dir, images[0]))
+    max_height, max_width, layers = first_image.shape
+
+video_size = (max_width, max_height)
 
 # Create output cv2 VideoWriter
 out = cv2.VideoWriter(output, cv2.VideoWriter_fourcc(*str(codec)), fr, video_size)
 
 # Write each image in the images array to the output video
-for i in images:
-    img = cv2.imread(os.path.join(dir, i))
-    out.write(img)
+if var:
+    for i in images:
+        img = cv2.imread(os.path.join(dir, i))
+        resized = cv2.resize(img, video_size)
+        out.write(resized)
+else:
+    for i in images:
+        img = cv2.imread(os.path.join(dir, i))
+        out.write(img)
 
 # Release the output video
 out.release()
